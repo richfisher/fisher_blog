7415c0fa26f824a7fcd5ba5edd2721cf8a9873bd
richfisher <richfisher.pan@gmail.com>
2013-05-08 19:08:06 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FisherBlog::Application.config.secret_token = '4c8711fad5adb939629ea599a1420e6cdfbc0f07ec3b94bbe7e4bd7413526407d2b37ecf789c4e6765d76d8b4dec5db256bbe5e0864ae03effc140980692e636'
