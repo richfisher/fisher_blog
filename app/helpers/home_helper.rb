41bdf36038f35d270e387edf5f845442ed2fa429
richfisher <richfisher.pan@gmail.com>
2013-05-13 12:00:58 +0800

    count tags

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,15 @@
 module HomeHelper
+	def get_tags
+		tags = Article.all.map{|e| e.tag_array}.inject([]){|s,e| s+=e;s}
+		tags.inject(Hash.new(0)){|h,e| h[e]+= 1; h}.sort_by {|k,v| v}.reverse
+	end
+
+	def render_tag_link(pair,index)
+		return if index > 10
+		font_size = 16-index 
+		font_size = 10 if font_size < 10
+		content_tag :span,:style=>"font-size:#{font_size}px" do 
+			pair[0]
+		end
+	end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
   attr_accessible :category_id, :content, :tags, :title
   attr_accessible :created_at, :updated_at
+
+  def tag_array
+  	self.tags.split(',') rescue []
+  end
 end
