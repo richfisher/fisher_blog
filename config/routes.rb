299533298e89931428876532f12a3b80f4f25378
richfisher <richfisher.pan@gmail.com>
2013-05-13 10:48:52 +0800

    add devise layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 FisherBlog::Application.routes.draw do
   devise_for :users
-
+  devise_scope :user do
+    get "login", :to => "devise/sessions#new"
+    get "logout", :to => "devise/sessions#destroy"
+  end
+  
   namespace :panel do
     resources :categories, :except=>[:show]
     resources :articles, :except=>[:show]
