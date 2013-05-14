79feae24b969efbb4764b13715d0fe6af8e91c14
richfisher <richfisher.pan@gmail.com>
2013-05-14 22:02:58 +0800

    create target database

--- config/recipes/mysql.rb
+++ config/recipes/mysql.rb
@@ -14,6 +14,13 @@ namespace :mysql do
     run "#{sudo} mysqladmin -uroot -p password '#{new_mysql_password}'",:pty => true do |ch, stream, data|
       smart_input(ch, stream, data)
     end
+
+    set(:new_db_name) { Capistrano::CLI.password_prompt("New db name: ") }
+    create_sql = <<-SQL
+      CREATE DATABASE #{new_db_name};
+    SQL
+
+    run "mysql --user=root --password='#{new_mysql_password}' --execute=\"#{create_sql}\""
   end
   # after "deploy:setup", "mysql:setup"
 
