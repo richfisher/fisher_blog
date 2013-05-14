fcd3cb5730943918430e4e390c4c86938eb65392
richfisher <richfisher.pan@gmail.com>
2013-05-14 22:25:31 +0800

    set title for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,7 @@ class CategoriesController < ApplicationController
 	def show
 		@category = Category.find(params[:id])
 		@articles = @category.articles.all
+
+    set_page_title "Category: #{@category.name}"
 	end
 end
