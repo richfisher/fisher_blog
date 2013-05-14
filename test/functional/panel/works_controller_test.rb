6f5850989f6b28b471ce223f63870391e5efcc7b
richfisher <richfisher.pan@gmail.com>
2013-05-14 22:10:33 +0800

    add panel works tests

--- test/functional/panel/works_controller_test.rb
+++ test/functional/panel/works_controller_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class Panel::WorksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @work = FactoryGirl.create(:work)
+    sign_in :user, FactoryGirl.create(:user) 
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:works)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create work" do
+    assert_difference('Work.count') do
+      post :create, work: {  }
+    end
+
+    assert_redirected_to panel_works_path
+  end
+
+  test "should get edit" do
+    get :edit, id: @work
+    assert_response :success
+  end
+
+  test "should update work" do
+    put :update, id: @work, work: {  }
+    assert_redirected_to panel_works_path
+  end
+
+  test "should destroy work" do
+    assert_difference('Work.count', -1) do
+      delete :destroy, id: @work
+    end
+
+    assert_redirected_to panel_works_path
+  end
 end
