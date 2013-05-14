5a57005177ea11c8fa5cfaf0be46a888d2c9c493
richfisher <richfisher.pan@gmail.com>
2013-05-14 18:45:10 +0800

    disable user timeable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, 
-  			 :trackable, :validatable, :lockable, :timeoutable
+  			 :trackable, :validatable, :lockable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
